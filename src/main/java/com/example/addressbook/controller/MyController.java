16632d6a705e976dc678a52fb3b9ce502cb04565
Yenleng <yenlengggggg@gmail.com>
2023-05-29 00:19:06 +0800

    add login page:

--- src/main/java/com/example/addressbook/controller/MyController.java
+++ src/main/java/com/example/addressbook/controller/MyController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.addressbook.model.Info;
 import com.example.addressbook.repository.InfoRedis;
@@ -19,25 +20,25 @@ import jakarta.validation.Valid;
 @Controller
 
 public class MyController {
-    
+
     @Autowired
     InfoRedis repository;
 
     @GetMapping("/home")
-    public String showHome(){
+    public String showHome() {
         return "home";
     }
 
     @GetMapping("/signup")
-    public String signUp(Model model){
+    public String signUp(Model model) {
         var userInfo = new Info();
         model.addAttribute("info", userInfo);
         return "signup";
     }
 
     @PostMapping("/signup/save")
-    public String save(@ModelAttribute @Valid Info userInfo, BindingResult bindingResult, Model model){
-        if(bindingResult.hasErrors()){
+    public String save(@ModelAttribute @Valid Info userInfo, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
             return "signup";
         }
         repository.saveInfo(userInfo);
@@ -45,25 +46,49 @@ public class MyController {
         return "save";
     }
 
-    @GetMapping("/userlist")
-    public String userlist(Model model){
-        List<Info> infos = repository.getAllInfo();
-        model.addAttribute("allinfo", infos);
-        //System.out.println(infos);
-        return "userlist";
-    }
+    // @GetMapping("/userlist")
+    // public String userlist(Model model){
+    // List<Info> infos = repository.getAllInfo();
+    //
+    // //System.out.println(infos);
+    // return "userlist";
+    // }
 
     @GetMapping("/user/{userid}")
-    public String showUser(Model model, @PathVariable String userid){
+    public String showUser(Model model, @PathVariable String userid) {
         Info user = new Info();
         user = repository.getInfoByID(userid);
-        
-        if(user==null){
+
+        if (user == null) {
             System.out.println(user);
             return "error";
         }
         model.addAttribute("user", user);
 
         return "user";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password,
+            Model model) {
+        System.out.println(username + ":" + password);
+        List<Info> infos = repository.getAllInfo();
+        for (Info info : infos) {
+            if (username.equals(info.getUserName())) {
+                if (password.equals(info.getPassword())) {
+                    model.addAttribute("allinfo", infos);
+                    return "userlist";
+                }
+
+                return "login";
+            }
+        }
+
+        return "login";
     }
 }
